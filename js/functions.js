098d357e1d9a8a6ef9c7abc34896f7517311d247
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-06-25 19:29:27 +0300

    испроавлено

--- js/functions.js
+++ js/functions.js
@@ -5,20 +5,23 @@
 90 - продолжительность встречи в минутах
 */
 const getMin = (time) => {
+  const MIN_IN_HOUR = 60;
   const [hour, min] = time.split(':').map((element) => Number(element));
-  return hour * 60 + min;
-}
+  return hour * MIN_IN_HOUR + min;
+};
 
 const beOnTime = (startTime, endTime, currentTime, lenTime) => {
   const startTimeMin = getMin(startTime);
   const endTimeMin = getMin(endTime);
   const currentTimeMin = getMin(currentTime);
-  console.log(startTimeMin, endTimeMin, currentTimeMin, lenTime);
-  return (startTimeMin <= currentTimeMin) && (endTimeMin >= currentTimeMin + lenTime);
-}
+  // console.log(startTimeMin, endTimeMin, currentTimeMin, lenTime);
 
-console.log(beOnTime('08:00', '17:30', '14:00', 90)); // true
-console.log(beOnTime('8:0', '10:0', '8:0', 120));     // true
-console.log(beOnTime('08:00', '14:30', '14:00', 90)); // false
-console.log(beOnTime('14:00', '17:30', '08:0', 90));  // false
-console.log(beOnTime('8:00', '17:30', '08:00', 900)); // false
+  return (startTimeMin <= currentTimeMin) && (endTimeMin >= currentTimeMin + lenTime);
+};
+
+beOnTime('08:00', '17:30', '14:00', 90);
+// console.log(beOnTime('08:00', '17:30', '14:00', 90)); // true
+// console.log(beOnTime('8:0', '10:0', '8:0', 120));     // true
+// console.log(beOnTime('08:00', '14:30', '14:00', 90)); // false
+// console.log(beOnTime('14:00', '17:30', '08:0', 90));  // false
+// console.log(beOnTime('8:00', '17:30', '08:00', 900)); // false
